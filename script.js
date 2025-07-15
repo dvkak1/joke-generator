dae46048ff4a39eaa6eacb45d29bbf660f0ca3b0
dvkak1 <dv_kakilala@yahoo.com>
2025-07-15 16:01:32 +0800

    joke javascript and api added

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const dadJokeEl = document.getElementById("joke");
-const jokeBtn = document.getElementById("joke-btn");
+const jokeBtn = document.getElementById("dad-joke-btn");
 
 
+const generateDadJoke = () => {
+   const xhr = new XMLHttpRequest();
+   xhr.open("GET", "https://api.chucknorris.io/jokes/random");
+
+      xhr.onreadystatechange = function() {
+         if (this.readyState === 4 && this.status === 200) {
+              dadJokeEl.innerHTML = JSON.parse(this.responseText).value;
+         } else {
+              dadJokeEl.innerHTML = "Chuck Joke brain not found. Am sorry!";
+         }
+    };
+
+   xhr.send();
+};
+
+jokeBtn.addEventListener("click", generateDadJoke);
+document.addEventListener("DOMContentLoaded", generateDadJoke);
